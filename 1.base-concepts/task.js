3a65c09679d9405c15902ad30f3790f2f7c274b1
doc5624 <doc5624@yandex.ru>
2024-08-29 01:09:24 +0700

    DZ 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let x;
+  let y;
+  let desc = b * b - 4 * a * c;
+  if (desc > 0) {
+    x = (-b + Math.sqrt(desc)) / (2 * a);
+    y = (-b - Math.sqrt(desc)) / (2 * a);
+    arr.push(x);
+    arr.push(y);
+  } else if (desc === 0) {
+    x = -b / (2 * a);
+    arr.push(x);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = 1/12 * percent / 100;
+  let s = amount - contribution
+  let Платёж = s * (p + (p / (((1 + p)**countMonths) - 1)));
+  let summ = Платёж * countMonths;
+  let sumString = (summ).toFixed(2);
+  let sumInt = parseFloat(sumString);
+  return sumInt;
 }
